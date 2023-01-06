ce8c1fef02ea0d3ca3db5a47fae09c0745053854
yasuhiromikuno <mikuno-0112@outlook.jp>
2023-01-06 01:39:33 +0000

    Progate学習

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Progate</title>
+  <link rel="stylesheet" href="stylesheet.css" type="text/css">
+</head>
+<body>
+  <?php
+  // 'Hello, World!'をechoしてください
+    echo 'Hello World!'
+  ?>
+
+  <br>
+
+  <?php
+  // 7 * 2をechoしてください
+    echo 7 * 2
+  ?>
+
+  <br>
+
+  <?php
+  // 8 % 3をechoしてください
+    echo 8 % 3
+  ?>
+
+</body>
+</html>
