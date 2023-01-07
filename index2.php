f7b29ed90e3fe3b11c2691f975e3b9c041572dce
yasuhiromikuno <mikuno-0112@outlook.jp>
2023-01-07 14:16:06 +0000

    [add]PHPの基礎を学ぼう_データの種類

---
+++ index2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Progate</title>
+</head>
+<body>
+  
+  <?php
+    echo 5 + 7
+  ?>
+  <br>
+  <?php
+    echo '5 + 7'
+  ?>
+  
+</body>
+</html>
