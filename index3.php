ab4f5a2b13758eb836e68801b1e9c1c7d23bdce0
yasuhiromikuno <mikuno-0112@outlook.jp>
2023-01-08 00:26:45 +0000

    [add]PHPの基礎を学ぼう_変数

---
+++ index3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Progate</title>
+</head>
+<body>
+  <? php
+    $fruit = 'りんご';
+    echo $fruit;
+  ?>
+
+  <br>
+
+  <? php
+    $sum = 8 + 9;
+    echo $sum;
+  ?>
+
+
+</body>
+</html>
