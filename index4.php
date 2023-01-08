f85b0f3eb4ccdc057d4f7c0b9e281e35d3a1c25c
yasuhiromikuno <mikuno-0112@outlook.jp>
2023-01-08 00:57:58 +0000

    [add]PHPの基礎を学ぼう_変数の値を更新する

---
+++ index4.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Progate</title>
+</head>
+<body>
+  <?php
+    $x = 5;
+    $y = 2;
+    $a = 8;
+    $b = 4;
+  ?>
+
+  <? php
+    $x += 10;
+    echo $x;
+  ?>
+
+  <br>
+
+  <? php
+    $y *= 5;
+    echo $y;
+  ?>
+
+  <br>
+
+  <? php
+    $a += 1;
+    echo $a;
+  ?>
+
+  <br>
+
+  <? php
+    $b -= 1;
+    echo $b;
+  ?>
+
+
+
+</body>
+</html>
