18e15369d10d238dfb6185a42967100696ff1275
yasuhiromikuno <mikuno-0112@outlook.jp>
2023-01-08 01:04:57 +0000

    [add]PHPの基礎を学ぼう_文字列の連結

---
+++ index5.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Progate</title>
+</head>
+<body>
+  <?php
+    $name = 'にんじゃわんこ';
+    echo "こんにちは！{$name}"
+  ?>
+
+
+
+</body>
+</html>
