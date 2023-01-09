bad3cdfc8d6ff7aefe4416dd1266c3d41ee16a56
yasuhiromikuno <mikuno-0112@outlook.jp>
2023-01-09 12:08:27 +0000

    [add]条件分岐_&&,||

---
+++ index8.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+
+    $x = 1071;
+
+    // 以下にif-elseif-else文を書いてください
+
+
+  ?>
+
+</body>
+</html>
