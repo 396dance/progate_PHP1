b5ae2210a96aae6651fbf1ce345bdf7df1422802
yasuhiromikuno <mikuno-0112@outlook.jp>
2023-01-13 12:14:44 +0000

    [add]繰り返し処理_while文

---
+++ index13.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+
+    // 変数$iを定義してください
+
+
+    // while文を書いてください
+
+
+  ?>
+
+</body>
+</html>
