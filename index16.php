07dad545fa997364b0ca3d98f0bd9484a89b911c
yasuhiromikuno <mikuno-0112@outlook.jp>
2023-01-17 13:23:40 +0000

    [add]繰り返し処理_foreach文

--- index16.php
+++ index16.php
@@ -12,7 +12,9 @@
     $scores = array('数学' => 70, '英語' => 90, '国語' => 80);
 
     // この下にforeach文を書いてください
-
+    foreach ( $scores as $key => $value ) {
+      echo $key.'は'.$value.'点です。';
+    }
 
   ?>
 
