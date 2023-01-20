d62e0ae2a78de1e9118bb78edac0baf17c339e76
yasuhiromikuno <mikuno-0112@outlook.jp>
2023-01-20 10:09:53 +0000

    [add]関数を使ってみよう_関数を自作する

---
+++ index18.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+
+    // 関数helloを定義してください
+
+
+    // 関数helloを呼び出してください
+
+
+    echo '<br>';
+    // 関数printRectangleAreaを定義してください
+
+
+    // 引数を(5, 10)としてprintRectangleAreaを呼び出してください
+
+
+  ?>
+
+</body>
+</html>
